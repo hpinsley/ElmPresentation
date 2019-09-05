3c2e6d46302b9c84100c92e5d5e1610d3def6d15
Howard Pinsley <hpinsley@gmail.com>
2019-09-05 06:43:10 -0400

    wip

--- GettingARandomNumber/src/Main.elm
+++ GettingARandomNumber/src/Main.elm
@@ -13,17 +13,25 @@ type alias Model =
     { randomNumber : Int
     , lowInputVal : String
     , highInputVal : String
+    , low: Int
+    , high: Int
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { randomNumber = 0
-      , lowInputVal = "1"
-      , highInputVal = "100"
-      }
-    , Cmd.none
-    )
+    let
+        low = 1
+        high = 50
+    in
+        ( { randomNumber = 0
+        , lowInputVal = toString low
+        , highInputVal = toString high
+        , low = low
+        , high = high
+        }
+        , Cmd.none
+        )
 
 
 
@@ -36,29 +44,14 @@ type Msg
     | LowInputChanged String
     | HighInputChanged String
 
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GetRandomNumber ->
             let
-                low =
-                    case String.toInt model.lowInputVal of
-                        Err _ ->
-                            1
-
-                        Ok n ->
-                            n
-
-                high =
-                    case String.toInt model.highInputVal of
-                        Err _ ->
-                            10
-
-                        Ok n ->
-                            n
-
                 cmd =
-                    generate GotRandomNumber (int low high)
+                    generate GotRandomNumber (int model.low model.high)
             in
                 ( model, cmd )
 
@@ -66,42 +59,55 @@ update msg model =
             ( { model | randomNumber = n }, Cmd.none )
 
         LowInputChanged val ->
-            ( { model | lowInputVal = val }, Cmd.none )
+            case String.toInt val of
+                Err _ -> (model, Cmd.none)
+                Ok v ->
+                    if (v <= model.high)
+                    then
+                        ( { model | lowInputVal = val, low = v }, Cmd.none )
+                    else
+                        ( model, Cmd.none)
 
         HighInputChanged val ->
-            ( { model | highInputVal = val }, Cmd.none )
+            case String.toInt val of
+                Err _ -> (model, Cmd.none)
+                Ok v ->
+                    if (v >= model.low)
+                    then
+                        ( { model | highInputVal = val, high = v }, Cmd.none )
+                    else
+                        (model, Cmd.none)
+
 
 ---- VIEW ----
+
+
+numberInput : String -> String -> (String -> Msg) -> Html Msg
+numberInput label_text val message =
+    div
+        []
+        [ label [] [ text label_text ]
+        , input
+            [ type_ "number"
+            , value val
+            , onInput message
+            ]
+            []
+        ]
 
 
 view : Model -> Html Msg
 view model =
     div [ style [ ( "font-size", "20pt" ), ( "margin-left", "20px" ) ] ]
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Getting a Random Number is Impure" ]
+        [ h1 [] [ text "Getting a Random Number is Impure" ]
         , div []
             [ text <|
                 "Your random number is "
                     ++ (toString model.randomNumber)
-            , div
-                []
-                [ label [] [ text "From:" ]
-                , input
-                    [ type_ "number"
-                    , value model.lowInputVal
-                    , onInput LowInputChanged
-                    ]
-                    []
-                ]
-            , div []
-                [ label [] [ text "To:" ]
-                , input
-                    [ type_ "number"
-                    , value model.highInputVal
-                    , onInput HighInputChanged
-                    ]
-                    []
-                ]
+            , numberInput "From:" model.lowInputVal LowInputChanged
+            , div [][text (toString model.low)]
+            , numberInput "To:" model.highInputVal HighInputChanged
+            , div [][text (toString model.high)]
             , div []
                 [ button [ onClick GetRandomNumber ] [ text "Get Number" ]
                 ]
